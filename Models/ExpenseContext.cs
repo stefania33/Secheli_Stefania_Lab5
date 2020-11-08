4d021d4334cc3d66eea8c7ff51034848d210ec44
Stefania <stefania.secheli99@gmail.com>
2020-11-08 16:20:26 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Secheli_Stefania_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expenses> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
---
+++ Models/Expenses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Secheli_Stefania_Lab5.Models
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Secret { get; set; }
+    }
+}
